76bb9462564c050449fa9fb353cad47fccd34a04
doni <donioctavian1016@gmail.com>
2021-05-04 18:57:28 +0700

    "Activity TicTacToe"

--- app/src/main/java/com/enigmacamp/andoridbasickotlinvicapremium/MainActivity.kt
+++ app/src/main/java/com/enigmacamp/andoridbasickotlinvicapremium/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.enigmacamp.andoridbasickotlinvicapremium
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun letsPlay(view: View) {
+        val intent = Intent(this,BoardActivity::class.java)
+        startActivity(intent)
+    }
+
+
 }
