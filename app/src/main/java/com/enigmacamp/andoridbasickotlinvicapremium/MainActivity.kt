ccc9204d0a4dddf03485addef6fed1b0618f0b30
doni <donioctavian1016@gmail.com>
2021-05-04 10:38:55 +0700

    "Activity Input Name"

--- app/src/main/java/com/enigmacamp/andoridbasickotlinvicapremium/MainActivity.kt
+++ app/src/main/java/com/enigmacamp/andoridbasickotlinvicapremium/MainActivity.kt
@@ -2,10 +2,24 @@ package com.enigmacamp.andoridbasickotlinvicapremium
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textview.MaterialTextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var textInputName : TextInputEditText
+    private lateinit var textViewName : MaterialTextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textInputName = findViewById(R.id.input_name)
+        textViewName = findViewById(R.id.text_view_name)
+    }
+
+    fun showName(view: View) {
+        val name = textInputName.text
+        textViewName.text = name
     }
 }
