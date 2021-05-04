cef6efa14fe4dccf6afe28b9fc3bc503bb9c38a1
doni <donioctavian1016@gmail.com>
2021-05-04 11:13:35 +0700

    "Activity Counter"

--- app/src/main/java/com/enigmacamp/andoridbasickotlinvicapremium/MainActivity.kt
+++ app/src/main/java/com/enigmacamp/andoridbasickotlinvicapremium/MainActivity.kt
@@ -2,10 +2,27 @@ package com.enigmacamp.andoridbasickotlinvicapremium
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.textview.MaterialTextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var textView : MaterialTextView
+    private var starCounter = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.text_view)
+    }
+
+    fun increase(view: View) {
+        //pakai ++ boleh atau pakai +=1 karena sama setiap valuenya akan bertamabah satu
+        starCounter+=1
+        textView.text = starCounter.toString()
+    }
+    fun decrease(view: View) {
+        starCounter-=1
+        textView.text = starCounter.toString()
     }
 }
