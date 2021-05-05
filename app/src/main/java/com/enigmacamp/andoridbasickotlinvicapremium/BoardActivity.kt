4a55b4317a3eabc108246a6f0b16eac6eca2b773
doni <donioctavian1016@gmail.com>
2021-05-05 09:44:27 +0700

    "Activity TicTacToe"

--- app/src/main/java/com/enigmacamp/andoridbasickotlinvicapremium/BoardActivity.kt
+++ app/src/main/java/com/enigmacamp/andoridbasickotlinvicapremium/BoardActivity.kt
@@ -2,10 +2,46 @@ package com.enigmacamp.andoridbasickotlinvicapremium
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import com.enigmacamp.andoridbasickotlinvicapremium.MainActivity.Companion.FIRST_NAME
+import com.enigmacamp.andoridbasickotlinvicapremium.MainActivity.Companion.LAST_NAME
 
 class BoardActivity : AppCompatActivity() {
+
+    private lateinit var playerName : TextView
+
+    private var activePlayer = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_board)
+
+        playerName = findViewById(R.id.textView3)
+
+        val playerOne = intent.getStringExtra(FIRST_NAME)
+        playerName.text = "${playerOne}'s Turn"
+    }
+
+    private fun playGame(buttonSelected: Button) {
+        val playerOne = intent.getStringExtra(FIRST_NAME)
+        val playerTwo = intent.getStringExtra(LAST_NAME)
+        if(activePlayer==1){
+            playerName.text = "$playerTwo's Turn"
+            buttonSelected.text = "X"
+            activePlayer = 2
+        }else{
+            playerName.text = "$playerOne's Turn"
+            buttonSelected.text = "O"
+            activePlayer = 1
+        }
+        //disable button, after click
+        buttonSelected.isEnabled = false;
+    }
+
+    fun buttonClick(view: View) {
+        val buttonSelected = view as Button
+        playGame(buttonSelected)
     }
 }
